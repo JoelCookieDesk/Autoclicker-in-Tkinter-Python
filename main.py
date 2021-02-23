b5ade803c491f473505b58414227f7c595c2170f
JoelCookieDesk <78533943+JoelCookieDesk@users.noreply.github.com>
2021-02-23 17:19:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,14 @@ import pyautogui, keyboard, time
 from tkinter import *
 app = Tk()
 app.title("Autoclicker")
-def leftClicking():
-    leftClickRunning = True
+def startAutoClicking():
+    running = True
     time.sleep(1)
-    while leftClickRunning == True:
-        if keyboard.is_pressed("L"):
-            leftClickRunning = False
+    while running == True:
+        if keyboard.is_pressed("B"):
+            running = False
             break
         pyautogui.leftClick(pyautogui.position())
-def rightClicking():
-    rightClickRunning = True
-    time.sleep(1)
-    while rightClickRunning == True:
-        if keyboard.is_pressed("R"):
-            rightClickRunning = False
-            break
-        pyautogui.rightClick(pyautogui.position())
-Label(app, text="Note: Press 'l' to stop left clicking and 'r' to stop right clicking.").pack()
-left_button = Button(app, text="Left Click", command=leftClicking)
-left_button.grid(row=1, column=0)
-right_button = Button(app, text="Right Click", command=rightClicking)
-right_button.grid(row=0, column=1)
+Label(app, text="Note: Press 'B' to stop clicking.").pack()
+left_button = Button(app, text="Left Click", command=startAutoClicking).pack()
 app.mainloop()
